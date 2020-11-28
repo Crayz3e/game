92ca1bf66fe03046b03d55478aced306d9a06103
mariyamsej <mrakovkarl@gmail.com>
2020-11-29 00:23:41 +0600

    towers

--- tower/tower.py
+++ tower/tower.py
@@ -1,4 +1,65 @@
-from abc import ABC
+import pygame
+import math
+import os
 
-class Tower(ABC):
-    def attack(self, )
+#from ??? import menu ???
+#у нас ведь будет меню для магазина, чтобы покупать башни?
+
+class Tower(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = 0
+        self.height = 0
+
+        self.selected = False
+        self.range = 0
+        self.tower_imgs = None
+        self.damage = 1
+        self.price = 0
+
+        #self.menu
+        self.place_color = (255, 0, 0)
+
+    def draw(self, win):
+        img = self.tower_imgs
+        win.blit(img, (self.x - img.get_width()//2, self.y - img.get_height()//2))
+        #вот тут вообще думала вызывать меню
+        #if self.selected:
+        #    self.menu.draw(win)
+
+    def clicked(self, x_pos, y_pos):
+        img = self.tower_imgs
+        if self.x - img.get_width()//2 + self.width >= x_pos and x_pos >= self.x - img.get_width()//2:
+            if self.y + self.height - img.get_height()//2 >= y_pos and y_pos >= self.y - img.get_height()//2:
+                return True
+        return False
+
+    #def attack_radius(self, win):
+    #нужно ли, чтобы область атаки подсвечивалась?
+
+    def draw_placement_range(self, win):
+        surface = pygame.Surface((self.range * 4, self.range * 4), pygame.SRCALPHA, 32)
+        pygame.draw.circle(surface, self.place_color, (50, 50), 50, 0)
+
+        win.blit(surface, (self.x - 50, self.y - 50))
+
+    def buy_and_pay(self):
+        return self.price
+
+    def move_tower(self, x, y):
+        self.x = x
+        self.y = y
+        self.menu.x = x
+        self.menu.y = y
+        self.menu.update()
+
+    def collide_other_tower(self, other_tower):
+        x2 = other_tower.x
+        y2 = other_tower.y
+
+        dis = math.sqrt((x2 - self.x)**2 + (y2 - self.y)**2)
+        if dis >= 100:
+            return False
+        else:
+            return True
---
+++ tower/tower_archer.py
@@ -0,0 +1,93 @@
+import pygame
+import os
+
+from Towers.tower import Tower
+
+import math
+
+archer_imgs1 = []
+
+for i in range(37, 44):
+    tower_imgs1 = pygame.transform.scale(pygame.image.load(os.path.join("images/game/archers", str(x) + ".png")).convert_alpha(), (64, 64))
+
+for i in range(2, 10):
+    archer_imgs1.append(pygame.image.load(os.path.join("images/game/arch_towers", str(x) + ".png")), convert_aplha(), (90, 90))
+
+
+class ArcherTower(Tower):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        #я взяла с того туториала на ютубе эту идею хд
+        self.tower_imgs = tower_imgs1
+        self.archer_imgs = archer_imgs1[:]
+        self.archer_count = 200 #сколько лучников?
+        self.range = 200
+        self.original_range = self.range
+        self.in_range = False
+        self.left = True
+        self.damage = 30
+        self.width = self.height = 90
+        self.original_damage = self.damage
+        self.moving = False
+        self.name = "archer"
+        #self.menu
+
+    #def get_cost(self):
+        #return self.menu.
+
+    def draw(self, win):
+        super().draw_range(win)
+        super().draw(win)
+
+        if self.in_range and not self.moving:
+            self.archer_count += 1
+            if self.archer_count >= len(self.archer_imgs) * 10:
+                self.archer_count = 0
+        else:
+            self.archer_count = 0
+
+        archer = self.archer_imgs[self.archer_count // 10]
+        if self.left is True:
+            add = -25
+        else:
+            add = -archer.get_width() + 10
+
+        win.blit(archer, ((self.x + add), (self.y - archer.get_height() - 25)))
+
+    def change_attack_range(self, rng):
+        self.range = rng
+
+    def attack(self, enemies):
+        money = 0
+        self.in_range = False
+        enemy_closest = []
+        for enemy in enemies:
+            x = enemy.x
+            y = enemy.y
+
+            dis = math.sqrt((self.x - 100 / 2 - x) ** 2 + (self.y - 100 / 2 - y) ** 2)
+            if dis < self.range:
+                self.in_range = True
+                enemy_closest.append(enemy)
+
+        enemy_closest.sort(key=lambda i: i.path_pos)
+        enemy_closest = enemy_closest[::-1]
+
+        if len(enemy_closest) > 0:
+            first_enemy = enemy_closest[0]
+            #
+            if self.atcher_count == :
+                if first_enemy.health(self.damage) is True:
+                    money = first_enemy.if_killed_money_earned * 2
+                    enemies.remove(first_enemy)
+
+            if first_enemy.x > self.x and not self.left:
+                self.left = True
+                for x, img in enumerate(self.archer_imgs):
+                    self.archer_imgs[x] = pygame.transform.flip(img, True, False)
+            elif self.left and first_enemy.x < self.x:
+                self.left = False
+                for x, img in enumerate(self.archer_imgs):
+                    self.archer_imgs[x] = pygame.transform.flip(img, True, False)
+
+        return money
